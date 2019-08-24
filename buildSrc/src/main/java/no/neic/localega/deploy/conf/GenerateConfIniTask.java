ba52ec754b0c61d823ee82f25e3b747e695d5bd8
Dmytro Titov <dmytro.titov@gmail.com>
2019-08-24 15:39:52 +0200

    Fix inbox location in conf.ini

--- buildSrc/src/main/java/no/neic/localega/deploy/conf/GenerateConfIniTask.java
+++ buildSrc/src/main/java/no/neic/localega/deploy/conf/GenerateConfIniTask.java
@@ -12,14 +12,12 @@ public class GenerateConfIniTask extends LocalEGATask {
 
     @TaskAction
     public void run() throws Exception {
-        String inboxLocation = System.getenv("INBOX_LOCATION");
         String vaultS3AccessKey = System.getenv("VAULT_S3_ACCESS_KEY");
         String vaultS3SecretKey = System.getenv("VAULT_S3_SECRET_KEY");
         String dbHost = System.getenv("DB_HOST");
         String dbPassword = System.getenv("DB_LEGA_IN_PASSWORD");
         String mqConnection = System.getenv("MQ_CONNECTION");
         String confIni = IOUtils.resourceToString("/default.conf.ini", Charset.defaultCharset());
-        confIni = confIni.replace("INBOX_LOCATION", String.valueOf(inboxLocation));
         confIni = confIni.replace("VAULT_S3_ACCESS_KEY", String.valueOf(vaultS3AccessKey));
         confIni = confIni.replace("VAULT_S3_SECRET_KEY", String.valueOf(vaultS3SecretKey));
         confIni = confIni.replace("DB_HOST", String.valueOf(dbHost));
