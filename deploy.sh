c4f1ffc73646cb21b3bf7d6b7fda2085b3b67bfd
Dmytro Titov <dmytro.titov@gmail.com>
2019-08-26 19:41:06 +0200

    Move out cleanup steps in a separate file

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  docker stack rm LEGA
-
-  sleep 30
-  docker volume rm LEGA_vault_s3
-  docker volume rm LEGA_db
-
-  docker config rm CA.cert
-  docker config rm client-server.cert
-  docker config rm client-server.key
-  docker config rm conf.ini
-  docker config rm ega.sec
-  docker config rm ega.sec.pass
-  docker config rm ega.pub
-  docker config rm ega.shared.pass
-
   gradle generateCertificate \
     -PsubjectString=C=NO,ST=Oslo,L=Oslo,O=UiO,OU=IFI,CN=nels-developers@googlegroups.com \
     -PfileName=client-server \
