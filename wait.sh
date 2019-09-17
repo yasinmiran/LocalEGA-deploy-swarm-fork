f61db311382085a7e634eca76f5e6c0106823660
Dmytro Titov <dmytro.titov@gmail.com>
2019-09-17 10:44:05 +0200

    Fix wait.sh

--- wait.sh
+++ wait.sh
@@ -13,16 +13,4 @@ if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     attempt_counter=$(($attempt_counter + 1))
     sleep 10
   done
-
-  attempt_counter=0
-  while [[ $(curl --insecure -s -o /dev/null -w "%{http_code}" https://$TSD_IP_ADDRESS:9001/minio/health/ready) != 200 ]]; do
-    if [ ${attempt_counter} -eq ${max_attempts} ]; then
-      printf "\nMax attempts reached\n"
-      exit 1
-    fi
-
-    printf '.'
-    attempt_counter=$(($attempt_counter + 1))
-    sleep 10
-  done
 fi
