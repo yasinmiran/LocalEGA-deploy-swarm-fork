88fa4737b685bd057439744a959cc333011dce48
Dmytro Titov <dmytro.titov@gmail.com>
2019-10-22 13:29:12 +0200

    Improve cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -4,15 +4,7 @@ if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   docker stack rm LEGA
 
   sleep 30
-  docker volume rm LEGA_vault_s3
-  docker volume rm LEGA_db
-
-  docker config rm CA.cert
-  docker config rm client-server.cert
-  docker config rm client-server.key
-  docker config rm conf.ini
-  docker config rm ega.sec
-  docker config rm ega.sec.pass
-  docker config rm ega.pub
-  docker config rm ega.shared.pass
+  docker rmi $(docker images -aq) || true
+  docker volume rm $(docker volume list -q) || true
+  docker config rm $(docker config list -q) || true
 fi
