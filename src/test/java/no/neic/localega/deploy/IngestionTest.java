51e3f0d7dc205c0ff585a6c092faf68ee7d5f187
dtitov <dmytro.titov@gmail.com>
2019-12-09 21:58:57 +0100

    Update IngestionTest

--- src/test/java/no/neic/localega/deploy/IngestionTest.java
+++ src/test/java/no/neic/localega/deploy/IngestionTest.java
@@ -4,9 +4,11 @@ import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import io.minio.MinioClient;
-import io.minio.errors.*;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
 import lombok.extern.slf4j.Slf4j;
+import no.uio.ifi.crypt4gh.stream.Crypt4GHOutputStream;
+import no.uio.ifi.crypt4gh.util.KeyUtils;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
@@ -14,19 +16,15 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.security.InvalidKeyException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -38,11 +36,15 @@ import java.util.concurrent.TimeoutException;
 @Slf4j
 public class IngestionTest {
 
+    private KeyUtils keyUtils = KeyUtils.getInstance();
+
     private File rawFile;
     private File encFile;
+    private KeyPair senderKeyPair;
+    private KeyPair recipientKeyPair;
 
     @Before
-    public void setup() throws IOException {
+    public void setup() throws IOException, GeneralSecurityException {
         long fileSize = 1024 * 1024 * 10;
         log.info("Generating " + fileSize + " bytes file to submit...");
         rawFile = new File(UUID.randomUUID().toString() + ".raw");
@@ -52,42 +54,69 @@ public class IngestionTest {
         byte[] bytes = DigestUtils.sha256(Files.newInputStream(rawFile.toPath()));
         log.info("Checksum: " + Hex.encodeHexString(bytes));
 
+        log.info("Generating sender and recipient key-pairs...");
+        senderKeyPair = keyUtils.generateKeyPair();
+        recipientKeyPair = keyUtils.generateKeyPair();
+
+
         log.info("Encrypting the file with Crypt4GH...");
         encFile = new File(rawFile.getName() + ".enc");
-        byte[] digest = DigestUtils.sha256(Files.newInputStream(rawFile.toPath()));
-        String key = FileUtils.readFileToString(new File("ega.pub"), Charset.defaultCharset().displayName());
+        PublicKey localEGAInstancePublicKey = keyUtils.readPublicKey(new File("ega.pub.pem"));
         FileOutputStream fileOutputStream = new FileOutputStream(encFile);
-//        Crypt4GHOutputStream crypt4GHOutputStream = new Crypt4GHOutputStream(fileOutputStream, key, digest);
-//        String sessionKey = Hex.encodeHexString(crypt4GHOutputStream.getSessionKeyBytes());
-//        String iv = Hex.encodeHexString(crypt4GHOutputStream.getIvBytes());
-//        log.info("Session key: " + sessionKey);
-//        log.info("IV: " + iv);
-//        FileUtils.copyFile(rawFile, crypt4GHOutputStream);
-//        crypt4GHOutputStream.close();
+        Crypt4GHOutputStream crypt4GHOutputStream = new Crypt4GHOutputStream(fileOutputStream, senderKeyPair.getPrivate(), localEGAInstancePublicKey);
+        FileUtils.copyFile(rawFile, crypt4GHOutputStream);
+        crypt4GHOutputStream.close();
     }
 
     @Test
     public void test() {
         try {
-            upload(System.getenv("S3_ENDPOINT"), System.getenv("INBOX_ACCESS_KEY"), System.getenv("INBOX_SECRET_KEY"));
-            ingest(System.getenv("CEGA_CONNECTION"));
+            upload();
+            ingest();
             Thread.sleep(10000); // wait for ingestion and verification to be finished
-            verify(System.getenv("TSD_IP_ADDRESS"));
+            verify();
         } catch (Throwable t) {
             log.error(t.getMessage(), t);
             Assert.fail();
         }
     }
 
-    private void upload(String s3Endpoint, String accessKey, String secretKey) throws IOException, InvalidPortException, InvalidEndpointException, InvalidKeyException, NoSuchAlgorithmException, InsufficientDataException, InvalidArgumentException, InternalException, NoResponseException, InvalidBucketNameException, XmlPullParserException, ErrorResponseException, InvalidResponseException {
-        log.info("Connecting to " + s3Endpoint);
-        MinioClient minioClient = new MinioClient(s3Endpoint, accessKey, secretKey, false);
-        log.info("Uploading a file...");
-        minioClient.putObject("inbox", "dummy/" + encFile.getName(), encFile.getAbsolutePath());
+    private void upload() throws IOException {
+        log.info("Uploading a file through a proxy...");
+        Unirest.config().verifySsl(false);
+        String token = Unirest
+                .get("https://localhost/cega")
+                .basicAuth("dummy", "dummy")
+                .asString()
+                .getBody();
+        String md5Hex = DigestUtils.md5Hex(Files.newInputStream(encFile.toPath()));
+        log.info("MD5 digest: {}", md5Hex);
+        String uploadURL = String.format("https://localhost/stream/%s?md5=%s", encFile.getName(), md5Hex);
+        JsonNode jsonResponse = Unirest
+                .patch(uploadURL)
+                .header("Authorization", "Bearer " + token)
+                .body(FileUtils.readFileToByteArray(encFile))
+                .asJson()
+                .getBody();
+        Assert.assertEquals(201, jsonResponse.getObject().getInt("statusCode"));
+        String uploadId = jsonResponse.getObject().getString("id");
+        log.info("Upload ID: {}", uploadId);
+        String finalizeURL = String.format("https://localhost/stream/%s?uploadId=%s&chunk=end&md5=%s&fileSize=%s",
+                encFile.getName(),
+                uploadId,
+                md5Hex,
+                FileUtils.sizeOf(encFile));
+        jsonResponse = Unirest
+                .patch(finalizeURL)
+                .header("Authorization", "Bearer " + token)
+                .asJson()
+                .getBody();
+        Assert.assertEquals(201, jsonResponse.getObject().getInt("statusCode"));
     }
 
-    private void ingest(String mqConnectionString) throws IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
+    private void ingest() throws IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
         log.info("Publishing ingestion message to CentralEGA...");
+        String mqConnectionString = System.getenv("CEGA_CONNECTION");
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUri(mqConnectionString);
         Connection connectionFactory = factory.newConnection();
@@ -113,8 +142,9 @@ public class IngestionTest {
     }
 
     @SuppressWarnings({"SqlResolve", "SqlNoDataSourceInspection"})
-    private void verify(String dbHost) throws SQLException {
+    private void verify() throws SQLException {
         log.info("Starting verification...");
+        String dbHost = "localhost";
         String port = "5432";
         String db = "lega";
         String url = String.format("jdbc:postgresql://%s:%s/%s", dbHost, port, db);
@@ -122,11 +152,11 @@ public class IngestionTest {
         props.setProperty("user", "lega_in");
         props.setProperty("password", System.getenv("DB_LEGA_IN_PASSWORD"));
         props.setProperty("ssl", "true");
-        props.setProperty("sslmode", "require");
         props.setProperty("application_name", "LocalEGA");
+        props.setProperty("sslmode", "verify-full");
         props.setProperty("sslrootcert", new File("rootCA.pem").getAbsolutePath());
-        props.setProperty("sslcert", new File("client-server.pem").getAbsolutePath());
-        props.setProperty("sslkey", new File("client-server-key.der").getAbsolutePath());
+        props.setProperty("sslcert", new File("localhost+6-client.pem").getAbsolutePath());
+        props.setProperty("sslkey", new File("localhost+6-client-key.der").getAbsolutePath());
         try {
             java.sql.Connection conn = DriverManager.getConnection(url, props);
             String sql = "select status from local_ega.files where status = 'COMPLETED' AND inbox_path = ?";
