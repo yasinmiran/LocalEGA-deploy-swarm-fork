e7cf4ae0e61193b0382145fd8c0071a7e3cc9c97
dtitov <dmytro.titov@gmail.com>
2020-02-24 15:18:25 +0100

    Install Crypt4GH with sudo

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ curl -L https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1
 chmod +x ~/mkcert
 shopt -s expand_aliases
 alias mkcert="~/mkcert"
-curl -fsSL https://raw.githubusercontent.com/elixir-oslo/crypt4gh/master/install.sh | sh
+curl -fsSL https://raw.githubusercontent.com/elixir-oslo/crypt4gh/master/install.sh | sudo sh
